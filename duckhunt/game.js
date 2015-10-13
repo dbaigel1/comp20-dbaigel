c59f0c2d00174f7f4418118231cd3a506bef240f
Daniel Baigel <dbaigel1@gmail.com>
2015-10-13 12:30:16 -0400

    took out extraneous comments

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -5,13 +5,13 @@ function init() {
 
     var canvas=document.getElementById("game_canvas");
     var ctx=canvas.getContext("2d");
-    //img=document.getElementById("game_canvas");
+    
     img.src = "duckhunt-background.gif";
     duck1.src = "duckhunt_various_sheet.png";
     duck2.src = "duckhunt_various_sheet.png";
     img.onload = function(){
     	ctx.drawImage(img, 0, 0);
-    	//375 x 267
+    
     	ctx.drawImage(duck1, 125, 110, 40, 40, 130, 30, 50, 50);
         ctx.drawImage(duck2, 35, 110, 40, 40, 70, 50, 50, 50);
 
